207df44cd2d1937917396d74eff8237afe2343b7
Daniel Vega <danielalexvega@gmail.com>
2020-10-23 17:24:05 -0500

    add lodash to StreamEdit. Use _.pick function

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
@@ -21,7 +22,7 @@ class StreamEdit extends React.Component {
         <h3>Edit a Stream</h3>
         <StreamForm 
           onSubmit={this.onSubmit}
-          initialValues={this.props.stream}/>
+          initialValues={_.pick(this.props.stream, 'title', 'description')}/>
       </div>
     );
   }
